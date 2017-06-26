064df88069ba2dd069e833d539292cf8011d4fe3
Ronny <ronnyli91@gmail.com>
2017-06-26 11:47:45 -0400

    snoowrap

--- popup.js
+++ popup.js
@@ -43,6 +43,7 @@ document.addEventListener('DOMContentLoaded', function() {
 // TODO: login does not persist across browser_action sessions
 var auth_flow = (function() {
   'use strict';
+  const snoowrap = require('snoowrap');
 
   var signin_button;
   var revoke_button;
@@ -56,25 +57,25 @@ var auth_flow = (function() {
     //       The app ID appears to change from one comp to next
     var redirectUri = chrome.identity.getRedirectURL('provider_cb');
     var redirectRe = new RegExp(redirectUri + '[#\?](.*)');
+    var userAgent = chrome.runtime.id + ':' + 'v0.0.1' + ' (by /u/sirius_li)'
 
     var access_token = null;
 
     return {
-      getToken: function(interactive, callback) {
-        // In case we already have an access_token cached, simply return it.
-        if (access_token) {
-          callback(null, access_token);
-          return;
-        }
+      getSnoowrap: function(interactive, callback) {
+        var authenticationUrl = snoowrap.getAuthUrl({
+          clientId: clientId,
+          scope: ['identity', 'submit'],
+          redirectUri: redirectUri,
+          permanent: true,
+          state: 'fe211bebc52eb3da9bef8db6e63104d3' // a random string, this could be validated when the user is redirected back
+        });
+        // --> 'https://www.reddit.com/api/v1/authorize?client_id=foobarbaz&response_type=code&state= ...'
+        console.log('Snoowrap auth URL:', authenticationUrl);
 
         var options = {
           'interactive': interactive,
-          'url': 'https://www.reddit.com/api/v1/authorize' +
-                 '?client_id=' + clientId +
-                 '&redirect_uri=' + encodeURIComponent(redirectUri) +
-                 '&response_type=token' +
-                 '&state=TODO' +
-                 '&scope=' + encodeURIComponent('identity submit')
+          'url': authenticationUrl
         }
         chrome.identity.launchWebAuthFlow(options, function(redirectUri) {
           console.log('launchWebAuthFlow completed', chrome.runtime.lastError,
@@ -91,70 +92,15 @@ var auth_flow = (function() {
           // or:
           // https://{app_id}.chromiumapp.org/provider_cb#code={value}
           var matches = redirectUri.match(redirectRe);
-          if (matches && matches.length > 1)
-            handleProviderResponse(parseRedirectFragment(matches[1]));
-          else
+          if (matches && matches.length > 1) {
+            var code = new URL(redirectUri).searchParams.get('code');
+            console.log('Snoowrap received code:', code);
+            console.log('fromAuthCode:', code, userAgent, clientId, redirectUri);
+            return callback(null, code);
+          } else {
             callback(new Error('Invalid redirect URI'));
+          }
         });
-
-        function parseRedirectFragment(fragment) {
-          var pairs = fragment.split(/&/);
-          var values = {};
-
-          pairs.forEach(function(pair) {
-            var nameval = pair.split(/=/);
-            values[nameval[0]] = nameval[1];
-          });
-
-          return values;
-        }
-
-        function handleProviderResponse(values) {
-          console.log('providerResponse', values);
-          if (values.hasOwnProperty('access_token'))
-            setAccessToken(values.access_token);
-          // If response does not have an access_token, it might have the code,
-          // which can be used in exchange for token.
-          else if (values.hasOwnProperty('code'))
-            exchangeCodeForToken(values.code);
-          else 
-            callback(new Error('Neither access_token nor code avialable.'));
-        }
-        // TODO: delete or modify this function
-        function exchangeCodeForToken(code) {
-          var xhr = new XMLHttpRequest();
-          xhr.open('GET',
-                   'https://github.com/login/oauth/access_token?' +
-                   'client_id=' + clientId +
-                   '&client_secret=' + clientSecret +
-                   '&redirect_uri=' + redirectUri +
-                   '&code=' + code);
-          xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-          xhr.setRequestHeader('Accept', 'application/json');
-          xhr.onload = function () {
-            // When exchanging code for token, the response comes as json, which
-            // can be easily parsed to an object.
-            if (this.status === 200) {
-              var response = JSON.parse(this.responseText);
-              console.log(response);
-              if (response.hasOwnProperty('access_token')) {
-                setAccessToken(response.access_token);
-              } else {
-                callback(new Error('Cannot obtain access_token from code.'));
-              }
-            } else {
-              console.log('code exchange status:', this.status);
-              callback(new Error('Code exchange failed'));
-            }
-          };
-          xhr.send();
-        }
-
-        function setAccessToken(token) {
-          access_token = token; 
-          console.log('Setting access_token: ', access_token);
-          callback(null, access_token);
-        }
       },
 
       removeCachedToken: function(token_to_remove) {
@@ -164,72 +110,23 @@ var auth_flow = (function() {
     }
   })();
 
-  function xhrWithAuth(method, url, interactive, callback) {
-    var retry = true;
-    var access_token;
-
-    console.log('xhrWithAuth', method, url, interactive);
-    getToken();
-
-    function getToken() {
-      tokenFetcher.getToken(interactive, function(error, token) {
-        console.log('token fetch', error, token);
-        if (error) {
-          callback(error);
-          return;
-        }
-
-        access_token = token;
-        requestStart();
-      });
-    }
-
-    function requestStart() {
-      var xhr = new XMLHttpRequest();
-      xhr.open(method, url);
-      xhr.setRequestHeader('Authorization', 'Bearer ' + access_token);
-      xhr.setRequestHeader('UserAgent', chrome.runtime.id + ':' + 'v0.0.1' + ' (by /u/sirius_li)');
-      xhr.onload = requestComplete;
-      xhr.send();
-    }
-
-    function requestComplete() {
-      console.log('requestComplete', this.status, this.response);
-      if ( ( this.status < 200 || this.status >=300 ) && retry) {
-        retry = false;
-        tokenFetcher.removeCachedToken(access_token);
-        access_token = null;
-        getToken();
-      } else {
-        callback(null, this.status, this.response);
-      }
-    }
-  }
-
   // API calls
 
-  function getUserInfo(interactive) {
-    xhrWithAuth('GET',
-                'https://oauth.reddit.com/api/v1/me',
-                interactive,
-                onUserInfoFetched);
-  }
-
-  function postSubmit(interactive) {
-    var url = 'https://oauth.reddit.com/api/v1/submit' +
-              '?api_type=json' +
-              '&kind=link' +
-              '&resubmit=true' +
-              '&sendreplies=true' +
-              '&sr=' + 'test' +
-              //'&text=' + '' +
-              '&title=' + 'test' +
-              '&url=' + encodeURIComponent('https://www.google.com')
-
-    xhrWithAuth('POST',
-                url,
-                interactive,
-                onSubmission);
+  function getUserInfo(interactive, token) {
+    var clientId = 'WVBdzQjziRt8jQ';
+    // TODO: Does the redirect URL on Reddit's side need to be programmatic?
+    //       The app ID appears to change from one comp to next
+    var redirectUri = chrome.identity.getRedirectURL('provider_cb');
+    var userAgent = chrome.runtime.id + ':' + 'v0.0.1' + ' (by /u/sirius_li)'
+    snoowrap.fromAuthCode({
+      code: token,
+      userAgent: userAgent,
+      clientId: clientId,
+      redirectUri: redirectUri
+    }).then(r => {
+      // Now we have a requester that can access reddit through the user's account
+      return r.getMe().then(onUserInfoFetched);
+    });
   }
 
   // Functions updating the User Interface:
@@ -247,17 +144,11 @@ var auth_flow = (function() {
     button.disabled = true;
   }
 
-  function onUserInfoFetched(error, status, response) {
-    if (!error && status == 200) {
-      console.log("Got the following user info: " + response);
-      var user_info = JSON.parse(response);
-      populateUserInfo(user_info);
-      hideButton(signin_button);
-      showButton(revoke_button);
-    } else {
-      console.log('infoFetch failed', error, status);
-      showButton(signin_button);
-    }
+  function onUserInfoFetched(response) {
+    console.log("Got the following user info: " + response);
+    populateUserInfo(response);
+    hideButton(signin_button);
+    showButton(revoke_button);
   }
 
   function onSubmission(error, status, response) {
@@ -269,11 +160,10 @@ var auth_flow = (function() {
     }
   }
 
-  function populateUserInfo(user_info) {
+  function populateUserInfo(reddituser) {
     var elem = user_info_div;
     var nameElem = document.createElement('div');
-    nameElem.innerHTML = "<b>Hello " + user_info.name + "</b><br>"
-      + "Your github page is: " + user_info.name;
+    nameElem.innerHTML = "<b>Hello " + reddituser.name + "</b>";
     elem.appendChild(nameElem);
   }
 
@@ -281,12 +171,11 @@ var auth_flow = (function() {
 
   function interactiveSignIn() {
     disableButton(signin_button);
-    tokenFetcher.getToken(true, function(error, access_token) {
+    tokenFetcher.getSnoowrap(true, function(error, token) {
       if (error) {
         showButton(signin_button);
       } else {
-        getUserInfo(true);
-        postSubmit(true);
+        getUserInfo(true, token);
       }
     });
   }
@@ -316,7 +205,7 @@ var auth_flow = (function() {
       console.log(signin_button, revoke_button, user_info_div);
 
       showButton(signin_button);
-      getUserInfo(false);
+      // getUserInfo(false); NO IDEA WHAT THIS DOES
     }
   };
 })();
